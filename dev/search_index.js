fafa9a4b974b969c376bba6be64362ad61dac1a0
Documenter.jl <documenter@juliadocs.github.io>
2023-08-02 02:37:44 +0000

    build based on 5b70dd9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ElectronLiquidRG","category":"page"},{"location":"#ElectronLiquidRG","page":"Home","title":"ElectronLiquidRG","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ElectronLiquidRG.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ElectronLiquidRG]","category":"page"}]
+}
