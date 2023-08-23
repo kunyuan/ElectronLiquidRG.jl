10bb5430afdf9978c05484ac47f6a5a352c990fc
Kun Chen <chenkun0228@gmail.com>
2023-08-23 11:47:12 -0400

    update data

--- experiment/MC.jl
+++ experiment/MC.jl
@@ -4,7 +4,7 @@ using ElectronLiquid
 neval = 4e9
 
 dim = 3
-rs = [8.0,]
+rs = [5.0,]
 mass2 = [0.001,]
 beta = [25.0]
 order = [1,]
@@ -16,9 +16,9 @@ for (_rs, _mass2, _beta, _order) in Iterators.product(rs, mass2, beta, order)
     # for _F in ElectronLiquidRG.fdict[_rs]
     # for _F in [ -0.3, -0.2, -0.1, 0.0]
     # for _F in [-1.2, -1.0, -0.8, -0.6, -0.4]
-    # for _F in [-1.0, -0.8, -0.6, -0.4]
+    for _F in [-1.0, -0.8, -0.6, -0.4]
     # for _F in [-2.0, -1.8, -1.6, -1.4, -1.2]
-    for _F in [-1.0, -0.8, -0.6, -0.4, -0.2, 0.0]
+    # for _F in [-1.0, -0.8, -0.6, -0.4, -0.2, 0.0]
         # for _F in [0.0, ]
         para = UEG.ParaMC(rs=_rs, beta=_beta, Fs=_F, order=_order, mass2=_mass2, isDynamic=isDynamic, dim=dim, isFock=isFock)
         kF = para.kF
